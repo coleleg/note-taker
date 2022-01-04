cb1fe8e5a231e549cef24643f38987591ac48a55
coleleg <coleleg@gmail.com>
2022-01-04 16:20:23 -0500

    server is up

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
 app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>  {
     console.log('API server now on port ${PORT} !');
