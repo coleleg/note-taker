ab775d47d87235c5a0ed13a5cd27e237159af648
coleleg <coleleg@gmail.com>
2022-03-15 20:10:49 -0400

    fix route functionality

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,33 +1,22 @@
 const router = require('express').Router();
-const { createNewNote, validateNote, filterByQuery, findById } = require('../../lib/notes');
+const fs = require('fs');
+// const { notes } = require('../../db/db.json');
+const { getNotes, createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
+
+
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    if (req.query) {
-        results = filterByQuery(req.query, results)
-    }
-    res.json(results);
-});
-
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if(result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
+    getNotes(notes);
+    res.json(notes);
+    return results;
 });
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
-
-    if(!validateNote(req.body)) {
-        res.status(400).send('This note is not formatted properly.');
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,15 +8,18 @@ const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
+// parse incoming JSON data
 app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () =>  {
-    console.log('API server now on port ${PORT} !');
+    console.log('API server now on port ' + PORT);
 });
